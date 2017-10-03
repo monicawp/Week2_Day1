042d5b9af1662fd32029631ba8d723274eb46919
monicawp <monicawperezf@gmail.com>
2017-10-03 10:09:51 -0400

    More Practice

--- Week2_Day1/Program.cs
+++ Week2_Day1/Program.cs
@@ -10,6 +10,83 @@ namespace Week2_Day1
     {
         static void Main(string[] args)
         {
-        }
+            //int[] favoriteNumbers = { 1, 2, 3, 4, 5, 6 };
+            //string[] studentNames = { "Raz", "Monica", "David", "Brian", "Ali" };
+
+            //Console.WriteLine(favoriteNumbers[0]);
+            //Console.WriteLine(studentNames[2]);
+
+            //string[] vacaSpots = { "Cancun", "Punta Cana", "Jamaica", "Bahamas", "Ibiza", "Thailand", "Santa Monica","Tulum", "Rome", "Venice" };
+            //int[] childAge = { 10, 17, 5 };
+            //decimal[] studentGpa = { 2.0m, 3.5m, 2.7m, 4.0m, 3.8m, 3.0m, 3.3m, 1.8m };
+            //char[] firstInitial = { 'M', 'R', 'K', 'B' };
+
+            //Console.WriteLine(vacaSpots[6] + " " + vacaSpots[1]);
+            //Console.WriteLine(childAge[0]);
+            //Console.WriteLine(studentGpa[3]);
+            //Console.WriteLine(firstInitial[0]);
+
+            //int[] numbers = new int[4];
+            //numbers[0] = 43;
+            //numbers[1] = 12;
+            //Console.WriteLine("Please enter a number");
+            //numbers[2] = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter a number");
+            //numbers[3] = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(numbers[3]);
+
+
+            string[] weekdays = new string[7];
+
+            //weekdays[0] = "Sunday";
+            //weekdays[1] = "Monday";
+            //weekdays[2] = "Tuesday";
+            //weekdays[3] = "Wednesday";
+            //weekdays[4] = "Thursday";
+            //weekdays[5] = "Friday";
+            //weekdays[6] = "Saturday";
+
+
+
+            //int[] studentAges = new int[5];
+            //studentAges[0] = 19;
+            //studentAges[1] = 20;
+            //studentAges[2] = 21;
+            //studentAges[3] = 22;
+            //studentAges[4] = 23;
+
+            //Console.WriteLine(weekdays.Length); 
+
+
+            //char[] lastname = new char[5];
+            //lastname[0] = 'P';
+            //lastname[1] = 'E';
+            //lastname[2] = 'R';
+            //lastname[3] = 'E';
+            //lastname[4] = 'Z';
+
+            //Console.WriteLine(lastname.Length);
+            //int index = lastname.Length;
+            //index = index - 2; 
+
+            //Console.WriteLine(lastname[index]);
+
+            //string[] strArray = { "This", "is", "an", "array", "of", "words", "because", "I", "like", "words"};
+
+            //Console.WriteLine("This is the last index " + (strArray.Length -1));
+            //Console.WriteLine(strArray[strArray.Length-2]);
+
+            int arraySize = int.Parse(Console.ReadLine());
+            int[] newArray = new int[arraySize];
+
+            Console.WriteLine("The array is of length " + newArray.Length);
+            
+                
+                
+                
+                
+                
+         }
     }
 }
